73ea47874dd02aa4b489a5d560c26a0c8ea5611e
Abhijeet <84558686+AJTimePyro@users.noreply.github.com>
2022-02-26 11:04:52 +0530

    Multiple channel added

--- config.py
+++ config.py
@@ -13,7 +13,11 @@ class Config(object):
     
     API_HASH = environ.get("API_HASH", "")
     
-    CHANNEL_ID = int(environ.get("CHANNEL_ID", None))
+    channel_id = environ.get("CHANNEL_ID", None)
+    if ',' in auth_channel:
+        CHANNEL_ID = list(map(int, list(channel_id.split(','))))
+    else:
+        CHANNEL_ID = int(channel_id)
 
     MONGO_STR = environ.get("MONGO_STR", "")
 
